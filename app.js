dfc86597d7b0067de789824de033a3af8c6bc268
idabagusangga <angga.idabagus@gmail.com>
2018-02-09 06:11:23 +0700

    mongoDB done, FBUser data added to mongoDB, update bodyType Done

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ var users = require('./routes/users');
 var bmi   = require('./routes/bmi')
 
 var app = express();
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb://angga:angga@ds135926.mlab.com:35926/library`);
+
+//db connection status
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('MLABS Connected');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));

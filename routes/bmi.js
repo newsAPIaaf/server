36ca78e3d191ac07f4129bdcbd298f13ffbc112d
fuadinaqi <fuadinaqi@aisifoundation.org>
2018-02-08 17:56:12 +0700

    change get to post router /bmi

--- routes/bmi.js
+++ routes/bmi.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 var controllerBMI = require('../controllers/controllerBMI')
 
-router.get('/', controllerBMI.getAll)
+router.post('/', controllerBMI.getAll)
 
 module.exports = router;

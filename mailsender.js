c739561a5eba49a5d296928f971c963559cf5972
angrha <angrha@gmail.com>
2018-02-09 14:11:09 +0700

    add mailsender raw

--- mailsender.js
+++ mailsender.js
@@ -24,106 +24,147 @@ class Mail {
       to     : this.emailTujuan, 
       subject: 'Enjoy create healthy foods' ,
       html   : `
-      <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-      <html xmlns="http://www.w3.org/1999/xhtml" 
-       xmlns:v="urn:schemas-microsoft-com:vml"
-       xmlns:o="urn:schemas-microsoft-com:office:office">
-      <head>
-      
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1"> 
-        <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
-        <meta name="format-detection" content="date=no"> 
-        <meta name="format-detection" content="telephone=no">
-        <title>Today's Weather</title>
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        rel="stylesheet">
-        <style type="text/css">
-      body {
-        margin: 0;
-        padding: 0;
-        -ms-text-size-adjust: 100%;
-        -webkit-text-size-adjust: 100%;
-      }
-      
-      table {
-        border-spacing: 0;
-      }
-      
-      table td {
-        border-collapse: collapse;
-      }
-      
-      .ExternalClass {
-        width: 100%;
-      }
-      
-      .ExternalClass,
-      .ExternalClass p,
-      .ExternalClass span,
-      .ExternalClass font,
-      .ExternalClass td,
-      .ExternalClass div {
-        line-height: 100%;
-      }
-      
-      .ReadMsgBody {
-        width: 100%;
-        background-color: #ebebeb;
-      }
-      
-      table {
-        mso-table-lspace: 0pt;
-        mso-table-rspace: 0pt;
-      }
-      
-      img {
-        -ms-interpolation-mode: bicubic;
-      }
-      
-      .yshortcuts a {
-        border-bottom: none !important;
-      }
-      
-      @media screen and (max-width: 599px) {
-        .force-row,
-        .container {
-          width: 100% !important;
-          max-width: 100% !important;
-        }
-      }
-      @media screen and (max-width: 400px) {
-        .container-padding {
-          padding-left: 12px !important;
-          padding-right: 12px !important;
-        }
-      }
-      .ios-footer a {
-        color: #aaaaaa !important;
-        text-decoration: underline;
-      }
-      a[href^="x-apple-data-detectors:"],
-      a[x-apple-data-detectors] {
-        color: inherit !important;
-        text-decoration: none !important;
-        font-size: inherit !important;
-        font-family: inherit !important;
-        font-weight: inherit !important;
-        line-height: inherit !important;
-      }
-      </style>
-      </head>
-      
-      <body style="margin:0; padding:0;" bgcolor="#F0F0F0" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
-      
-      <h1> name ${this.name} </h1>
-      <h1> image ${this.image} </h1>
-      <h1> ingredient ${this.ingredient} </h1>
-      <h1> diet ${this.diet} </h1>
-      <h1> health ${this.health} </h1>
-      
-      </body>
-      </html>`
+      <table class="full-width-container" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" bgcolor="#eeeeee" style="width: 100%; height: 100%; padding: 30px 0 30px 0;">
+      <tr>
+        <td align="center" valign="top">
+          <!-- / 700px container -->
+          <table class="container" border="0" cellpadding="0" cellspacing="0" width="700" bgcolor="#ffffff" style="width: 700px;">
+            <tr>
+              <td align="center" valign="top">
+  
+                <!-- / Hero subheader -->
+                <table class="container hero-subheader" border="0" cellpadding="0" cellspacing="0" width="620" style="width: 620px;">
+                  <tr>
+                    <td class="hero-subheader__title" style="font-size: 43px; font-weight: bold; padding: 80px 0 15px 0;" align="left">Product Design Portfolio</td>
+                  </tr>
+                </table>
+                <!-- /// Hero subheader -->
+  
+  
+                <!-- / Half block -->
+                <table class="container half-block" border="0" cellpadding="0" cellspacing="0" width="100%" style="padding-top: 25px;">
+                  <tr>
+                    <td>
+                      <table class="container" border="0" cellpadding="0" cellspacing="0" width="100%">
+                        <tr>
+                          <td class="half-block__image" style="width: 353px; height: 325px;" width="353" height="325" background="http://dev2.slicejack.com/portfolio-email/img/img14.jpg">
+                          <!--[if gte mso 9]>
+                          <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="width: 353px; height: 325px;" width="353" height="325">
+                          <v:fill type="frame" src="http://dev2.slicejack.com/portfolio-email/img/img14.jpg" color="#488bd3" />
+                          <v:textbox inset="0,0,0,0">
+                          <![endif]-->
+  
+                          <!--[if gte mso 9]>
+                          </v:textbox>
+                          </v:rect>
+                          <![endif]-->
+                          </td>
+  
+                          <td class="half-block__content" style="width: 50%; padding: 0 25px 0 25px; font-size: 16px; line-height: 27px; color: #969696; text-align: center;">
+                            ${this.name}, ${this.ingredient}, ${this.image}, ${this.diet}, ${this.health}
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                </table>
+                <!-- /// Half block -->
+  
+                <!-- / Half block -->
+                <table class="container half-block" border="0" cellpadding="0" cellspacing="0" width="100%">
+                  <tr>
+                    <td>
+                      <table class="container" border="0" cellpadding="0" cellspacing="0" width="100%">
+                        <tr>
+                          <td class="half-block__content" style="width: 50%; padding: 0 25px 0 25px; font-size: 16px; line-height: 27px; color: #969696; text-align: center;">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.
+                          </td>
+  
+                          <td class="half-block__image" style="width: 353px; height: 325px;" width="353" height="325" background="http://dev2.slicejack.com/portfolio-email/img/img15.jpg">
+                          <!--[if gte mso 9]>
+                          <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="width: 353px; height: 325px;" width="353" height="325">
+                          <v:fill type="frame" src="http://dev2.slicejack.com/portfolio-email/img/img15.jpg" color="#488bd3" />
+                          <v:textbox inset="0,0,0,0">
+                          <![endif]-->
+  
+                          <!--[if gte mso 9]>
+                          </v:textbox>
+                          </v:rect>
+                          <![endif]-->
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                </table>
+                <!-- /// Half block -->
+  
+                <!-- / Divider -->
+                <table class="container" border="0" cellpadding="0" cellspacing="0" width="100%" style="padding-top: 25px;" align="center">
+                  <tr>
+                    <td align="center">
+                      <table class="container" border="0" cellpadding="0" cellspacing="0" width="620" align="center" style="border-bottom: solid 1px #eeeeee; width: 620px;">
+                        <tr>
+                          <td align="center"> </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                </table>
+                <!-- /// Divider -->
+  
+                <!-- / CTA Block -->
+                <table class="container cta-block" border="0" cellpadding="0" cellspacing="0" width="100%">
+                  <tr>
+                    <td align="center" valign="top">
+                      <table class="container" border="0" cellpadding="0" cellspacing="0" width="620" style="width: 620px;">
+                        <tr>
+                          <td class="cta-block__title" style="padding: 35px 0 0 0; font-size: 26px; text-align: center;">About Us</td>
+                        </tr>
+  
+                        <tr>
+                          <td class="cta-block__content" style="padding: 20px 0 27px 0; font-size: 16px; line-height: 27px; color: #969696; text-align: center;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                          consequat.</td>
+                        </tr>
+  
+                        <tr>
+                          <td align="center">
+                            <table class="container" border="0" cellpadding="0" cellspacing="0">
+                              <tr>
+                                <td class="cta-block__button" width="230" align="center" style="width: 200px;">
+                                  <a href="#" style="border: 3px solid #eeeeee; color: #969696; text-decoration: none; padding: 15px 45px; text-transform: uppercase; display: block; text-align: center; font-size: 16px;">Visit Us</a>
+                                </td>
+                              </tr>
+                            </table>
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                </table>
+                <!-- /// CTA Block -->
+  
+                <!-- / Divider -->
+                <table class="container" border="0" cellpadding="0" cellspacing="0" width="100%" style="padding-top: 25px;" align="center">
+                  <tr>
+                    <td align="center">
+                      <table class="container" border="0" cellpadding="0" cellspacing="0" width="620" align="center" style="border-bottom: solid 1px #eeeeee; width: 620px;">
+                        <tr>
+                          <td align="center"> </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                </table>
+                <!-- /// Divider -->
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>`
     };
 
     transporter.sendMail(mailOptions, function (err, info) {
